9fe9d7b5cde6f0592ea906281c565c286fa86e50
evandalong <evandalong@gmail.com>
2017-11-09 11:18:55 -0500

    Fix test issue with phantamanta44/repeat-counter

--- phantamanta44/repeat-counter.js
+++ phantamanta44/repeat-counter.js
@@ -1,15 +1,18 @@
-const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_$';
+const chars = ['abcdefghijklmnopqrstuvwxyz$', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_'];
 for (let i = 0; i < 100; i++) {
   const max = Math.floor(Math.random() * 9) + 2;
   const aLen = Math.floor(Math.random() * 5);
   const bLen = Math.floor(Math.random() * 5);
-  let str = '';
+  let str = '', x = 0;
   for (let j = 0; j < aLen; j++) {
-    str += chars[Math.floor(Math.random() * chars.length)].repeat(Math.floor(Math.random() * max));
+    str += chars[x][Math.floor(Math.random() * 27)].repeat(Math.floor(Math.random() * max));
+    x ^= 1;
   }
-  str += chars[Math.floor(Math.random() * chars.length)].repeat(max);
+  str += chars[x][Math.floor(Math.random() * 27)].repeat(max);
+  x ^= 1;
   for (let j = 0; j < bLen; j++) {
-    str += chars[Math.floor(Math.random() * chars.length)].repeat(Math.floor(Math.random() * max));
+    str += chars[x][Math.floor(Math.random() * 27)].repeat(Math.floor(Math.random() * max));
+    x ^= 1;
   }
   Test.assertEquals(max, repeats(str));
 }
